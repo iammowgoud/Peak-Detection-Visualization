8e6b260ce939bcc17049af142e76b14652baedec
HatemHassan-xo <mowgoud@aucegypt.edu>
2019-10-23 05:19:23 +0200

    API adjustments

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
@@ -9,6 +10,7 @@ const io = socketIO(server);
 const port = process.env.PORT || 4001;
 
 const INTERVAL = 2000;
+const sensorData = require('./data.json');
 
 app.use(express.static(__dirname));
 
@@ -21,7 +23,6 @@ io.on('connection', (socket) => {
 
   io.connections[connectionId] = {
     sensorId,
-    init: true,
     index: 0,
     interval: setInterval(() => emitData(connectionId, socket), INTERVAL),
   }
@@ -40,8 +41,19 @@ server.listen(port, () => {
 
 emitData = (connectionId, socket) => {
   let conn = io.connections[connectionId]
-  console.log(`emitted to sensor id:${conn.sensor}, index:  ${conn.index}, init: ${conn.init}`);
-  socket.emit("chartData", ` to sensor id:${conn.sensorId}, index:  ${conn.index}, init: ${conn.init}`)
-  conn.init = false;
-  conn.index++;
+  console.log(`emitted to sensor id:${conn.sensorId}, index:  ${conn.index}`);
+    const { newIndex, response } = getNextReading(sensorData[conn.sensorId-1], conn.index )
+  socket.emit("reading", JSON.stringify(response));
+    conn.index = newIndex;
+}
+
+
+getNextReading = (data, index) => {
+  response = {
+    timestamp: Date.now(),
+    value: data.readings[index],
+    zscore: data.zScores[index]
+  };
+
+  return { newIndex: (index + 1) % data.readings.length, response  };
 }
