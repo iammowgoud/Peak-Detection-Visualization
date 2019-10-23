0ea8ddda5b7b0a67e25367229134e0750ab26e06
HatemHassan-xo <mowgoud@aucegypt.edu>
2019-10-23 07:10:33 +0200

    Include generated public folder

---
+++ api/public/precache-manifest.8eab6ce3b37af46e552b26e48fbef0d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a674e5431f692b28431bb6684ad74db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b83d02cfce7377fe56b1",
+    "url": "/static/css/main.041d8efd.chunk.css"
+  },
+  {
+    "revision": "7db953134822a407a1dd",
+    "url": "/static/js/2.ab99bcd8.chunk.js"
+  },
+  {
+    "revision": "b83d02cfce7377fe56b1",
+    "url": "/static/js/main.0461d84b.chunk.js"
+  },
+  {
+    "revision": "adbd3f96e5298877f768",
+    "url": "/static/js/runtime-main.792c7d5a.js"
+  }
+]);
