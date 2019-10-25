50e8d0112350941e1f7cbc07c9ac283c113a7f51
HatemHassan-xo <mowgoud@aucegypt.edu>
2019-10-25 13:15:20 +0200

    extension Fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.scss';
-import Chart from './components/chart';
+import Chart from './components/chart.js';
 
 ReactDOM.render(
   <div>
