4e88b0c670a72001abdb9f08ef6678a9c172e166
HatemHassan-xo <mowgoud@aucegypt.edu>
2019-10-25 07:56:21 +0200

    scss organization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.scss';
+import './styles/main.scss';
 import Chart from './components/Chart';
 
 ReactDOM.render(
