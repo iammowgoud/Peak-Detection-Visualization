971e806451956d2ad3bb677db833104071204e0c
HatemHassan-xo <mowgoud@aucegypt.edu>
2019-10-29 13:51:20 +0100

    Add links to blog and github

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import Chart from './components/Chart';
 
 ReactDOM.render(
   <div>
+    <a href="https://github.com/iammowgoud/Peak-Detection-Visualization" target="_blank" rel="noopener noreferrer">[ GitHub ]</a>
+    <a href="https://hatem-hassan.com/blog/Realtime-Data-Visualization-with-Peak-Detection-using-SocketIO-D3-and-React/" target="_blank" rel="noopener noreferrer">[ Research ]</a>
+    
     <h1>Peak Detection Dashboard</h1>
+    
     <Chart sensorId="1" />
     <Chart sensorId="2" x-ticks="20" />
     <Chart sensorId="3" x-ticks="20"/>
